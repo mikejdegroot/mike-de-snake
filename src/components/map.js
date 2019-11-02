d3796ff06e0fc848306db6a8476a9dd6c0bcc621
Mike de Groot <mike.j.degroot@gmail.com>
2019-11-02 17:52:04 +0000

    basic grid

--- src/components/map.js
+++ src/components/map.js
@@ -3,12 +3,34 @@ import Snake from './snake';
 
 
 export default class Map extends Component {
-  componentDidMount(){}
+  state = {
+    gridSize: 10,
+    blocks: []
+  }
+
+  componentDidMount(){
+    const { gridSize } = this.state;
+    let i = 0;
+    let row = 0;
+    while (i <= gridSize) {
+      i += 1;
+      const blocks = { coloumn: i };
+      this.setState(prevState => ({ blocks: [...prevState.blocks, blocks]}))
+    }
+  }
+
+  renderGrid = () => {
+    const { blocks } = this.state;
+    let i = 0;
+    return blocks.map(block => (
+      <Snake />
+    ))
+  };
 
   render() {
     return (
       <div className="map">
-        <Snake />
+        {this.renderGrid()}
       </div>
     )
   }
